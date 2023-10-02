7f2ec5cf0ae1a8c73d948f24fa3c5725460c8a95
Unknown <periswamy0003@gmail.com>
2023-10-02 21:32:14 +0530

    test

--- ggletxttspcs.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-import playsound
-import os
-spec="gotta go"
-tts=gTTS(spec,lang='ta',tld='co.in')
-tts.save("audio.mp3")
-playsound.playsound("audio.mp3")
-os.remove('audio.mp3')
